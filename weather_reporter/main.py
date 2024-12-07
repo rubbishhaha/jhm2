e130bae011551b7be1db84f5485ef0e7989185f6
rubbishhaha <xiaolaji444@gmail.com>
2024-12-07 02:02:45 +0000

    u

--- weather_reporter/main.py
+++ weather_reporter/main.py
@@ -2,4 +2,5 @@ import requests
 import json
 #response = requests.get("https://data.weather.gov.hk/weatherAPI/opendata/weather.php")
 response = requests.get("http://api.open-notify.org/astros")
-print(json.dumps(response.json(), sort_keys=True, indent=4))
+#print(json.dumps(response.json(), sort_keys=True, indent=4))
+print(response)
