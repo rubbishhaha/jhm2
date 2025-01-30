098503e3b1155a625c1850c510ce03954b5664d7
rubbishhaha <xiaolaji444@gmail.com>
2025-01-30 21:55:33 +0800

    shooting frame finish

--- shooting_game/main.py
+++ shooting_game/main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from pygame.locals import *
 from config import *
 from sprite import *
@@ -8,24 +9,30 @@ class game:
         self.bg = pygame.display.set_mode((b,h))
         self.FPS = pygame.time.Clock()
         self.close_window = False
-        self.status = "menu"
-        self.bg_image = pygame.transform.scale(pygame.image.load("asset/bg.png"),(b,h))
-        self.time = 0
-        pygame.display.set_caption("cnmb nigger")
+        self.status = "ui"
+        self.free_random = 0
+        pygame.display.set_caption("simple shooting game")
 
         self.UI_sprite = pygame.sprite.LayeredUpdates()
         self.main_sprite = pygame.sprite.LayeredUpdates()
         self.all_sprite = pygame.sprite.LayeredUpdates()
         self.main_enemy_sprite = pygame.sprite.Group()
         self.main_teammate_sprite = pygame.sprite.Group()
-        self.sp_sprite = pygame.sprite.LayeredUpdates()
-
-        self.player = player(self,"asset/player.png",(0,0))
-
+    
+    def main__init__(self):
+        self.bg_image = pygame.transform.scale(pygame.image.load("asset/bg.png"),(b,h))
+        self.player = player(self,"asset/player.png",(300,400))
+        self.time = 0
+        self.free_random = 0
+    def main__term__(self):
+        self.main_sprite.empty()
+        self.all_sprite.empty()
+        self.main_enemy_sprite.empty()
+        self.main_teammate_sprite.empty()
     def main(self):
         self.event()
-        self.draw()
-        self.update()
+        self.main_draw()
+        self.main_update()
         self.FPS.tick(FPS)
         pygame.display.update()
     def event(self):
@@ -36,55 +43,119 @@ class game:
                 if event.key==K_ESCAPE:
                     self.ESC_get_pressed()
         self.command = pygame.key.get_pressed()
-    def update(self):
+        self.mouse_codr = pygame.mouse.get_pos()
+        self.mouse_pressed = pygame.mouse.get_pressed()
+    def main_update(self):
         self.main_sprite.update()
-        self.sp_sprite.update()
         self.sprite_exist()
         self.enemy_spawn()
         self.time += 1
     def enemy_spawn(self):
-        sim_enemy(self,"asset/sine_enemy.png",(random.randint(0,600),-49),(30,30),health=20,moving="sine_curve",sines=[8,0.2],speed=4,type="sine_enemy"),
+        enemy_spawner.continous_spawning_checking()
         if self.time%(round(FPS/ENEMY_SPAWN_RATE)) == 0:
-            self.a1 = random.randint(0,2)
-            if self.a1 == 0:
-                sim_enemy(self,"asset/heavy_enemy.png",(random.randint(0,600),-49),(100,100),health=150,speed=1,type="heavy_enemy"),
-            elif self.a1 == 1:
-                sim_enemy(self,"asset/sine_enemy.png",(random.randint(0,600),-49),(30,30),health=20,moving="sine_curve",sines=[8,0.2],speed=4,type="sine_enemy"),
-            elif self.a1 == 2:
-                sim_enemy(self,"asset/shooting_enemy.png",(random.randint(0,600),-49),(50,50),health=70,speed=2,shooting=True,bullet="sim_bullet",type="shooting_enemy")
+            self.a1 = random.randint(0,4)
+            match self.a1:
+                case 0:
+                    sim_enemy(self,"asset/heavy_enemy.png",(random.randint(0,b),-49),(100,100),health=150,speed=1,type="heavy_enemy"),
+                case 1:
+                    sim_enemy(self,"asset/sine_enemy.png",(random.randint(0,b),-49),(30,30),health=20,moving="sine_curve",sines=[8,0.2],speed=4,type="sine_enemy"),
+                case 2:
+                    sim_enemy(self,"asset/shooting_enemy.png",(random.randint(0,b),-49),(50,50),health=70,speed=2,shooting=True,bullet="sim_bullet",bullet_arg={"speed":10,"direction":270},type="shooting_enemy")
+                case 3:
+                    enemy_spawner.sine_enemy_team(self)
+                case 4:
+                    enemy_spawner.plain_trapper(self)
 
     def sprite_exist(self):
-        for sprite in self.all_sprite:
+        for sprite in self.main_sprite:
             if sprite.asdf == False:
                 sprite.kill()
             if sprite.exist == False:
                 sprite.asdf = False
-    def draw(self):
+    def main_draw(self):
         self.bg.blit(self.bg_image, (0, 0,600,600))
         self.main_sprite.draw(self.bg)
-    def main_run__init__(self):
-        pass
+    def UI_draw(self):
+        if self.bluring_time > 0:
+            self.bluring_factor = self.bluring_time/OPTION_APPEAR_TIME
+            self.bg_image = pygame.transform.smoothscale(pygame.transform.smoothscale(self.bg_image_copy,((b-BACKGROUND_BLUR_RATE)*self.bluring_factor,(h-BACKGROUND_BLUR_RATE)*self.bluring_factor)),(b,h))
+            self.bluring_time += -1
+        self.bg.blit(self.bg_image, (0, 0,600,600))
+        self.UI_sprite.draw(self.bg)
     def UI(self):
-        pass
+        self.event()
+        self.UI_draw()
+        self.UI_update()
+        self.FPS.tick(FPS)
+        pygame.display.update()
     def UI__init__(self):
-        pass
+        self.bg_image = pygame.transform.scale(pygame.image.load("asset/bg.png"),(b,h))
+        self.bg_image.blit(pygame.image.load("asset/shoot_only.png"),(40,200))
+        option(self,[180,400,167,91],"asset/option_spreadsheet.png",0,"main",image_no=2)
+        self.bluring_time = 0
+    def UI__term__(self):
+        self.UI_sprite.empty()
+        self.bg_image = pygame.transform.scale(pygame.image.load("asset/bg.png"),(b,h))
+    def UI_update(self):
+        self.UI_sprite.update()
+    def menu__init__(self):
+        self.bg_image_copy = self.bg.copy()
+        self.bluring_time = OPTION_APPEAR_TIME
+        option(self,[13,255,167,91],"asset/option_spreadsheet.png",0,"ui",image_no=0,apppear_method="boom")
+        option(self,[180,255,167,91],"asset/option_spreadsheet.png",1,"main",image_no=1,apppear_method="boom")
+        option(self,[347,255,167,91],"asset/option_spreadsheet.png",0,"main",image_no=2,apppear_method="boom")
+
     def ESC_get_pressed(self):
         if self.status == "main":
             self.status = "menu"
-            self.UI__init__()
         elif self.status == "menu":
             self.status = "main"
-            self.main_run__init__()
+        if self.status == "ui":
+            self.close_window = True
+
+class enemy_spawner():
+    def continous_spawning_checking():
+        for u in spawn_list:
+            u[4] += 1
+            if u[4] >= u[3]:
+                if type(u[1][-1]) == dict:
+                    u[0](*u[1][:-1],**u[1][-1])
+                else:
+                    u[0](*u[1])
+                u[4] = 0
+                u[2] += -1
+                if u[2] <= 0:
+                    spawn_list.remove(u)
+    def continous_spawning_appending(func,data,time,num):
+        spawn_list.append([func,data,num,time,0])
+
+    def sine_enemy_team(self):
+        self.free_random = random.randint(0,b)
+        enemy_spawner.continous_spawning_appending(sim_enemy,[self,"asset/sine_enemy.png",(self.free_random,-49),(30,30),{"health":20,"moving":"sine_curve","sines":[8,0.07],"speed":4,"type":"sine_enemy"}],3,10)
+    def plain_trapper(self):
+        sim_enemy(self,"asset/trapper_enemy.png",(random.randint(0,b),-49),(70,70),health=30,speed=3,shooting=True,shooting_method="poly",shooting_method_c1=[2,0,180],bullet="lazer",bullet_arg={"speed":80,"hit_function":"invincible"},shooting_rate=15,type="shooting_enemy")
+
 
 pygame.init()
 game_state = game()
 
 while game_state.close_window == False:
-    game_state.main()
-    if game_state.status == "main":
-        game_state.main_run()
-    elif game_state.status == "menu":
+    game_state.UI__init__()
+    while game_state.status == "ui" and game_state.close_window == False:
         game_state.UI()
-    
+    game_state.UI__term__()
+    game_state.main__init__()
+    while game_state.status == "main" and game_state.close_window == False:
+        game_state.main()
+        if game_state.status == "menu":
+            game_state.menu__init__()
+            while game_state.status == "menu" and game_state.close_window == False:
+                game_state.UI()
+            game_state.UI__term__()
+    game_state.main__term__()
+
 
 pygame.quit()
+sys.exit()
+
+# ui init, ui run, ui destroy, main init, main run, esc init,esc run , esc destroy, main destroy
--- shooting_game/sprite.py
+++ shooting_game/sprite.py
@@ -9,7 +9,7 @@ class player(pygame.sprite.Sprite):
         self.exist = True
         self.asdf = True
         self.init_image = pygame.transform.scale(pygame.image.load(png),[30,30]).convert()
-        #self.init_image.set_alpha(PLAYER_TRANSPARENT_CONSTANT)
+        self.init_image.set_alpha(PLAYER_TRANSPARENT_CONSTANT)
         self.image = self.init_image
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect(topleft=init_pos)
@@ -108,20 +108,17 @@ class player(pygame.sprite.Sprite):
         self.a5_rect = self.a4_Im.get_rect(topleft=nr.topleft)
         return self.a4_Im, self.a5_rect
     def shoot(self,rect,mode):
-        if pygame.mouse.get_pressed()[0]:
+        if self.game.mouse_pressed[0]:
             if mode == 1:
                 random_rect = rect.copy()
                 random_rect.move_ip(random.randint(-30,30),random.randint(-30,30))
-                sim_bullet(self.game,"asset/player_mode1_bullet.png",random_rect.center, (37*PLAYER_BULLET_RADIO,286*PLAYER_BULLET_RADIO),self.groups,40,speed=40)
+                sim_bullet(self.game,"asset/player_mode1_bullet.png",random_rect.center, (286*PLAYER_BULLET_RADIO,37*PLAYER_BULLET_RADIO),self.groups,4,speed=40)
 class sim_bullet(pygame.sprite.Sprite):
     def __init__(self, game, png, init_pos, scale, groups,damage,**kwargs):
         self.exist = True
         self.asdf = True
-        self.image = pygame.transform.scale(pygame.image.load(png),scale).convert()
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect(center=init_pos)
-        self.game = game
         self.damage = damage
+        self.game = game
         self.arg_dict = {
             "direction":90,
             "hit_function": "explosion",
@@ -130,17 +127,16 @@ class sim_bullet(pygame.sprite.Sprite):
         }
         for i,val in kwargs.items():
             self.arg_dict[i] = val
-        self._layer = FX_LAYER 
+        self._layer = FX_LAYER
         self.groups = groups
         pygame.sprite.Sprite.__init__(self,groups)
 
         targeting(self)
 
         self.velocity = [self.arg_dict["speed"]*math.cos(math.radians(self.arg_dict["direction"])),-self.arg_dict["speed"]*math.sin(math.radians(self.arg_dict["direction"]))]
-        self.image = pygame.transform.scale(pygame.transform.rotate(pygame.image.load(png),self.arg_dict["direction"]),scale).convert()
+        self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(png),scale),self.arg_dict["direction"]).convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect(center=init_pos)
-        self.game = game
 
     def update(self, *args, **kwargs):
         self.move()
@@ -149,24 +145,30 @@ class sim_bullet(pygame.sprite.Sprite):
     def move(self):
         self.rect[0] += self.velocity[0]
         self.rect[1] += self.velocity[1]
-        if rect_in_game_area(self.rect) == False:
+        if rect_in_game_area_x(self.rect) == False or rect_in_game_area_y(self.rect) == False:
             self.exist = False
     def explosion(self):
         self.exist = False
         fx_wave(self.game,self.rect.center,20,100,0.3,BLUE)
     def hit(self):
-        if self.arg_dict["hit_function"] == "explosion":
-            self.explosion()
-        else:
-            self.exist = False
+        match self.arg_dict["hit_function"]:
+            case "explosion":
+                self.explosion()
+            case "invincible":
+                pass
 def targeting(self):
     if self.game.main_enemy_sprite.has(self):
         self.target = self.game.main_teammate_sprite
     elif self.game.main_teammate_sprite.has(self):
         self.target = self.game.main_enemy_sprite
         
-def rect_in_game_area(rect):
-    if rect[0] > b + 100 or rect[0] < -100 or rect[1] > h + 100 or rect[1] < -100:
+def rect_in_game_area_x(rect):
+    if rect.center[0] > b + 100 or rect.center[0] < -100:
+        return False
+    else:
+        return True
+def rect_in_game_area_y(rect):
+    if rect.center[1] > h + 100 or rect.center[1] < -100:
         return False
     else:
         return True
@@ -190,7 +192,10 @@ class sim_enemy(pygame.sprite.Sprite):
             "moving":"straight_line",
             "sines":False,
             "shooting":True,
+            "shooting_method":"",
             "bullet":None,
+            "bullet_arg":None,
+            "shooting_rate":ENEMY_SHOOTING_RATE,
             "type":"sim_enemy"
         }
         for i,val in kwargs.items():
@@ -211,7 +216,7 @@ class sim_enemy(pygame.sprite.Sprite):
         if get_hit(self):
             self.hit()
         if self.arg_dict["shooting"]:
-            self.shooting()
+            self.shooting_mth()
         self.sp_action()
         self.time += 1
         #pygame.draw.rect(self.game.bg,BLUE,self.rect,2)
@@ -221,7 +226,7 @@ class sim_enemy(pygame.sprite.Sprite):
         if self.arg_dict["moving"] == "sine_curve":
             self.rect[0] += self.arg_dict["sines"][0]*math.sin(self.time*self.arg_dict["sines"][1])
 
-        if rect_in_game_area(self.rect) == False:
+        if rect_in_game_area_y(self.rect) == False:
             self.exist = False
     def hit(self):
         for i in get_hit(self):
@@ -230,12 +235,24 @@ class sim_enemy(pygame.sprite.Sprite):
                 self.exist = False
                 fx_wave(self.game,self.rect.center,50,250,0.5,RED)
     def shooting(self):
-        if self.time%(round(FPS/ENEMY_SHOOTING_RATE)) == 0:
-            if self.arg_dict["bullet"] == "sim_bullet":
-                sim_bullet(self.game,"asset/enemy_bullet.png",self.rect.center, (100*ENEMY_BULLET_RADIO,256*ENEMY_BULLET_RADIO),self.groups,3,speed=10,direction=270)
+            match self.arg_dict["bullet"]:
+                case "sim_bullet":
+                    sim_bullet(self.game,"asset/enemy_bullet.png",self.rect.center, (256*ENEMY_BULLET_RADIO,100*ENEMY_BULLET_RADIO),self.groups,3,**self.arg_dict["bullet_arg"])
+                case "lazer":
+                    sim_bullet(self.game,"asset/enemy_bullet.png",self.rect.center, (8192*ENEMY_BULLET_RADIO,100*ENEMY_BULLET_RADIO),self.groups,1,**self.arg_dict["bullet_arg"])
     def sp_action(self):
         if self.arg_dict["type"] == "sine_enemy":
             self.image = pygame.transform.rotate(self.i_image,6.28*self.arg_dict["sines"][0]*math.sin(self.time*self.arg_dict["sines"][1]))
+    def shooting_mth(self):
+        if self.time%(round(FPS/self.arg_dict["shooting_rate"])) == 0:
+            match self.arg_dict["shooting_method"]:
+                case "":
+                    self.shooting()
+                case "poly":
+                    for i in range(self.arg_dict["shooting_method_c1"][0]):
+                        self.arg_dict["bullet_arg"]["direction"] = self.arg_dict["shooting_method_c1"][1]+i*self.arg_dict["shooting_method_c1"][2]
+                        self.shooting()
+
 
 class fx_wave(pygame.sprite.Sprite):
     def __init__(self,game,pos,i_s,t_s,t,color):
@@ -265,6 +282,87 @@ class fx_wave(pygame.sprite.Sprite):
         self.time += 1
         if self.time > self.frame:
             self.exist = False
+
+class option(pygame.sprite.Sprite):
+    def __init__(self,game,rect,image,func,value,image_no=-1,apppear_method=None):
+        self.game = game
+        self.rect = pygame.rect.Rect(rect)
+        self.r_rect = self.rect.copy()
+        if image_no != -1:
+            self.image = pygame.Surface((self.rect[2],self.rect[3])).convert()
+            self.a = pygame.image.load(image)
+            self.image.blit(self.a,(0,0),(167*image_no,0,self.rect[2],self.rect[3]))
+        else:
+            self.image = pygame.image.load(image).convert()
+        self.image.set_colorkey(BLACK)
+        self.r_image = self.image
+        match apppear_method:
+            case None:
+                self.appear_time = 0
+            case "boom":
+                self.appear_time = OPTION_APPEAR_TIME
+                self.appear_method = "boom"
+                self.image = pygame.Surface((0,0))
+        self.func = func
+        self.value = value
+        self._layer = FX_LAYER 
+        self.groups = game.all_sprite,game.UI_sprite
+        self.grown = False
+        self.g_x = (OPTION_GROW_RATIO-1)*self.r_rect[2]
+        self.g_Y = (OPTION_GROW_RATIO-1)*self.r_rect[3]
+        pygame.sprite.Sprite.__init__(self,self.groups)
+    def update(self, *args, **kwargs):
+        match self.mouse_get_pressed():
+            case 0:
+                self.de_grow()
+            case 1:
+                self.grow()
+            case 2:
+                self.execute()
+        if self.appear_time > 0:
+            self.cg_in()
+    def mouse_get_pressed(self):
+        if self.r_rect.collidepoint(self.game.mouse_codr):
+            if self.game.mouse_pressed[0]:
+                return 2
+            else:
+                return 1
+        else:
+            return 0
+    def grow(self):
+        self.n_x = math.floor(self.g_x-self.rect[2]+self.r_rect[2])/2
+        self.n_y = math.floor(self.g_x-self.rect[3]+self.r_rect[3])/2
+        if self.n_x <= 1:
+            self.n_x = 0
+            self.rect = pygame.Rect(self.r_rect[0]-self.g_x/2,self.r_rect[1]-self.g_Y/2,self.r_rect[2]+self.g_x,self.r_rect[3]+self.g_Y)
+        if self.n_y <= 1:
+            self.n_y = 0
+        self.rect = pygame.rect.Rect(self.rect[0]-self.n_x/2,self.rect[1]-self.n_y/2,self.rect[2]+self.n_x,self.rect[3]+self.n_y)
+        self.image = pygame.transform.scale(self.r_image,(self.rect[2],self.rect[3]))
         
-        
+    def de_grow(self):
+        self.n_x = math.floor(-self.rect[2]+self.r_rect[2])/2
+        self.n_y = math.floor(-self.rect[3]+self.r_rect[3])/2
+        if self.n_x >= -1:
+            self.n_x = 0
+            self.rect = self.r_rect
+        if self.n_y >= -1:
+            self.n_y = 0
+        self.rect = pygame.rect.Rect(self.rect[0]-self.n_x/2,self.rect[1]-self.n_y/2,self.rect[2]+self.n_x,self.rect[3]+self.n_y)
+        self.image = pygame.transform.scale(self.r_image,(self.rect[2],self.rect[3]))
+    def execute(self):
+        match self.func:
+            case 0:
+                self.game.status = self.value
+            case 1:
+                self.game.status = self.value
+                self.game.main__term__()
+                self.game.main__init__()
+    def cg_in(self):
+        match self.appear_method:
+            case "boom":
+                self.a1 = (OPTION_APPEAR_TIME-self.appear_time)/OPTION_APPEAR_TIME
+                self.image = pygame.transform.scale(self.r_image,(self.r_rect[2]*self.a1,self.r_rect[3]*self.a1))
+                self.appear_time += -1
+    
         
